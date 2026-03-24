f8572e37b884f4caa503b95329a1938c78712562
v.shilaev <valentin.shilaev@smk-systems.ru>
2026-03-24 10:31:37 +0300

    fix log4j2.xml fix build.gradle - resource path

--- src/main/java/ru/shilaev/automation/Main.java
+++ src/main/java/ru/shilaev/automation/Main.java
@@ -1,12 +1,19 @@
 package ru.shilaev.automation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.shilaev.automation.config.EnvironmentConfig;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException {
+        String deviceName = EnvironmentConfig.getDeviceName();
+        String environmentName = EnvironmentConfig.getEnvironmentName();
+
+        System.out.println(deviceName);
+        System.out.println(environmentName);
     }
 }
--- src/main/java/ru/shilaev/automation/config/EnvironmentConfig.java
+++ src/main/java/ru/shilaev/automation/config/EnvironmentConfig.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.shilaev.automation.core.exceptions.ConfigException;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
